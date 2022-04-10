f36262301dad9c969d575855577cebb4a5db4005
bara <barahana13@naver.com>
2022-04-10 22:15:05 +0900

    d

--- src/markdown_func.py
+++ src/markdown_func.py
@@ -9,12 +9,9 @@ def aa(tr_href, tr_bbs_num, tr_date):
   soup = BeautifulSoup(res.text, 'html.parser') 
   tr_title = soup.find('div', attrs={'class':'kboard-title'}).text
   tr_title = tr_title.replace('  ', ' ')
-  # soup.
-  # h = markdownify.markdownify(soup.currentTag, heading_style="ATX")
+
   h = markdownify.markdownify(res.text, heading_style="ATX")
   h = str(h)
-  # print(h)
-  # exit(1)
   h = h.replace('/_files/userfile/image', 'https://computer.knu.ac.kr/_files/userfile/image')
   comp_link = 'https://computer.knu.ac.kr/06_sub/02_sub.html'
   comp_down_link = "https://computer.knu.ac.kr/pack/bbs/down.php"
@@ -26,13 +23,8 @@ def aa(tr_href, tr_bbs_num, tr_date):
   if(h.find(comp_down_link)!=-1):
     c = re.compile(comp_down_link+r'.+Site_BBS_25')
     a = re.findall(c, h)
-    
     for a1 in a:
       h = h.replace(a1, a1.replace(' ', ''))
-    print(h)
+    h = h.replace(']첨부파일', ']\n첨부파일')
   with open(f'./storage/{str(tr_bbs_num)}_{tr_date}.md', 'w', encoding='utf-8') as f:
-      
-    # f.write(h[h.index(tr_title):h.index("$(document).on('ready',function () {")])
-    
     f.write(h)
-  # print(h)
