45898ae7987c65a637588a9108ee429ce7d8fa6f
barahana13 <barahana13@naver.com>
2022-04-14 00:59:37 +0900

    create replace_easy_visually_by_2week

--- src/fileutils.py
+++ src/fileutils.py
@@ -7,10 +7,11 @@ def createDir(directory):
     except OSError:
         print('Error: Creating directory. ' + directory)
         exit(1)
-        
+
 def write_to_file(path, str):
+    replace_easy_visually = lambda x: x.replace('\n\n\n\n', '\n\n').replace('\n\n\n\n\n', '\n\n')
     with open(join(path), 'w', encoding='utf-8') as f:
-        f.write(str[4:])
+        f.write(replace_easy_visually(str))
 
 def write_into(directory):
     def wrapper(path, str):
