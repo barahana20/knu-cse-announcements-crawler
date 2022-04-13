3c1936797a2256d624c42b7128369f8814712739
Insik-Won <insikwon@naver.com>
2022-04-14 00:50:09 +0900

    Refactor all of the codes

--- src/comp_crawling.py
+++ src/comp_crawling.py
@@ -1,90 +1,3 @@
-from distutils.debug import DEBUG
-import os
-import re as regex
-from collections import namedtuple
-import requests
-from bs4 import BeautifulSoup
-from markdownify import markdownify
-from functional import seq
-from fileutils import *
-from datetime import datetime
-
-DEBUG_MODE = 1
-
-comp_notice_url = "https://computer.knu.ac.kr/06_sub/02_sub.html?page={}&key=&keyfield=&category=&bbs_code=Site_BBS_25"
-storage_path = './storage'
-mile_path = './mile'
-storage_path = './markdown'
-NoticeInfo = namedtuple('NoticeInfo', 'link id title timestamp body source')
-
-def get_cse_notices(comp_notice_url):
-    res = requests.get(comp_notice_url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    notices = soup.find_all('tr')  # 일반 공지사항 목록 가져옴
-
-    return notices
-
-def is_announcement(notice):
-    if(str(notice).find('bbs_num') != -1):
-        id = notice.find('td', attrs={'class': 'bbs_num'})  # 공지사항 패스
-        if(id != None):  # 일반 공지인 경우 추가 탐색
-            return False
-    return True
-
-def get_info_from_notice(notice):
-    """
-    #  글 제목, 글 작성자
-    if(str(notice).find('font')!=-1):
-        print('정상작동')
-        title = notice.find('font').text
-    else:
-        print('에러')
-    
-    writer = notice.find('td', attrs={'class':'bbs_writer'}).text
-    """
-    [span.extract() for span in notice.select('span')]
-    
-    id = notice.find('td', attrs={'class': 'bbs_num'}).text
-    title = notice.find('a').text.strip()
-    link = 'https://computer.knu.ac.kr/06_sub/02_sub.html' + notice.find('a')['href']
-    
-    res = requests.get(link)
-    soup = BeautifulSoup(res.text, 'html.parser')
-
-    source = soup.find('div', attrs={'id': 'kboard-document'}).text
-    body = soup.find('div', attrs= {'class':'content-view'}).text
-
-    timestamp = (soup
-        .find('div', attrs={'class':'detail-attr detail-date'})
-        .find('div', attrs={'class':'detail-value'}).text
-    )
-    timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M")
-    
-    return NoticeInfo(link, id, title, timestamp, body, source)
-
-def filename_from_info(info):
-    _, id, title, timestamp, _, _ = info
-    timestamp = timestamp.strftime("%Y-%m-%dT%H-%M")
-    return to_allowed_filename(f'{id}_{title}_{timestamp}')+'.md'
-
-def make_md(source):
-    comp_link = 'https://computer.knu.ac.kr/06_sub/02_sub.html'
-    comp_down_link = "https://computer.knu.ac.kr/pack/bbs/down.php"
-    
-    document = str(markdownify(source, heading_style="ATX"))
-    document = (document
-                .replace('/_files/userfile/image', 'https://computer.knu.ac.kr/_files/userfile/image')
-                .replace('/pack/bbs/down.php', comp_down_link)
-                .replace('?key', comp_link+'?key')
-                .replace('?bbs_cmd', comp_link+'?bbs_cmd')
-                .replace(']첨부파일', ']  \n\n첨부파일') # 첨부파일 줄바꿈
-               )
-    
-    for match in regex.finditer(r'o_name=.+?&', document):
-        start, end = match.start(), match.end()
-        document = document[:start] + document[start:end].replace(' ', '') + document[end:]
-
-    return document
 '''
 함수형으로 코드를 짤려고 노력
     예시:
@@ -106,52 +19,12 @@ storage/년/월 디렉토리를 만들고
 '''
 
 """
-⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️추가해야할 것⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
-
-    1. 마일리지라는 문구가 있으면 마일리지 디렉토리 안에도 파일 저장
-    2. 갱신된 공지사항을 어떻게 감지해 낼 건지
-
-"""
-
-"""
-대충 방향성에 대해서 설명하자면
 1. 새로운 공지사항을 발견했을 때 등록된 사용자 이메일로 메일을 보냄
 2. 모든 올라오는 공지사항을 저장후 최근 1주일 간의 데이터들과 마일리지 정보들을 카카오톡 챗봇에게 요청하면 그것들을 출력함.
 """
+
+from notice import Notice, NoticeFactory
+from fileutils import write_into, subfiles, createDir
+
 if __name__ == '__main__':
-    createDir(storage_path)
-    createDir(mile_path)
-    create(body_path)
-    
-    get_timestamp_from_filename = lambda name: (
-        datetime.strptime(
-            name[name.rfind('_')+1:name.index('.md')], "%Y-%m-%dT%H-%M")
-    )
-    is_in_a_week = lambda timestamp: timestamp > 
-
-    recent_timestamp = (seq(subfiles(storage_path))
-        .map(get_timestamp_from_filename)
-        .sorted(reverse=True)
-        .first()
-    )
-        
-    is_newer_than_recent = lambda info: info.timestamp > recent_timestamp
-    info_to_filename_and_makrdown = lambda info: (filename_from_info(info), make_md(info.source))
-    write_to_storage = lambda filename_and_document: write_into(storage_path)(*filename_and_document)
-
-    notices = get_cse_notices(comp_notice_url.format('1'))
-    (seq(notices)
-        .filter_not(is_announcement)
-        .map(get_info_from_notice)
-        .filter(is_newer_than_recent)
-        #.map(info_to_filename_and_makrdown)
-        .for_each(lambda info: print(info.id))
-    )
-    
-    '''
-    for notice in get_cse_notices(comp_notice_url.format(1)):
-        if(is_announcement(notice)):
-            continue
-        id, link, date = get_info_from_notice(notice)
-        make_md(link, f'{id}_{date}.md', storage_path, mile_path)
-    '''
+    pass
--- src/notice.py
+++ src/notice.py
@@ -1,5 +1,137 @@
-from bs4 import BeautifulSoup
+from typing import Optional
+from typing_extensions import assert_never
+from lazy_property import LazyProperty
+
+from datetime import datetime
+import re as regex
+import json
+
+import requests
+from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup, Tag
+from bs4.element import Tag as Element
+from markdownify import markdownify
+
 
 class Notice:
-  def __init__(self, ):
-    pass
+  '''
+  Notice of KNU CSE official notice post
+  example: https://computer.knu.ac.kr/06_sub/02_sub.html?no=3739&bbs_cmd=view
+  '''
+  post_url = 'https://computer.knu.ac.kr/06_sub/02_sub.html' 
+  down_url = "https://computer.knu.ac.kr/pack/bbs/down.php"
+
+  def __init__(self, url: str, is_announcement: bool = False):
+    self.__url = url
+    self.__is_announcement = is_announcement
+
+    query = parse_qs(urlparse(url).query)
+    self.id = int(query['no'])
+
+  @LazyProperty
+  def __soup(self) -> BeautifulSoup:
+    response = requests.get(self.url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+  
+  @property
+  def url(self) -> str:
+    return f'{Notice.post_url}?no={self.id}&bbs_cmd=view'
+  
+  @property
+  def title(self) -> str:
+    title = self.__soup.find('div', attrs={'class': 'kboard-title'})
+    return title.text
+  
+  @LazyProperty
+  def timestamp(self) -> datetime:
+    timestamp = (self.__soup
+        .find('div', attrs={'class':'detail-attr detail-date'})
+        .find('div', attrs={'class':'detail-value'}).text
+    )
+    return datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
+
+  @property
+  def body(self) -> str:
+    return self.__soup.find('div', attrs={'class':'content-view'}).text
+  
+  @property
+  def is_announcement(self) -> bool:
+    return self.is_announcement
+
+  @property
+  def has_mileage(self) -> bool:
+    return '마일리지' in self.title or '마일리지' in self.body
+  
+  def to_markdown(self) -> str:
+    '''
+    convert Notice HTML code into markdown
+    '''
+    source = self.__soup.find('div', attrs={'id': 'kboard-document'})
+    document = str(markdownify(source, heading_style="ATX"))
+    document = (document
+                .replace('/_files/userfile/image', 'https://computer.knu.ac.kr/_files/userfile/image')
+                .replace('/pack/bbs/down.php', Notice.down_url)
+                .replace('?key', Notice.post_url+'?key')
+                .replace('?bbs_cmd', Notice.post_url+'?bbs_cmd')
+                .replace(']첨부파일', ']  \n\n첨부파일') # 첨부파일 줄바꿈
+               )
+
+    for match in regex.finditer(r'o_name=.+?&', document):
+      start, end = match.start(), match.end()
+      document = document[:start] + document[start:end].replace(' ', '') + document[end:]
+    
+    return document
+
+  def to_json(self) -> str:
+    source = {
+      self.id, self.title, self.timestamp, self.body
+    }
+    return json.dumps(source, indent=2, sort_keys=True)
+
+  def __str__(self) -> str:
+    return '\n'.join([
+     f'url: {self.url}',
+     f'\ttitle: {self.title[:10]}...',
+     f'\tid: {self.id}',
+     f'\ttimestamp: {self.timestamp}',
+     f'\tbody: {self.body[:10]}...'
+    ]).expandtabs(4)
+  
+  def __repr__(self) -> str:
+    return '\n'.join([
+     f'url: {self.url}',
+     f'\ttitle: {self.title}...',
+     f'\tid: {self.id}',
+     f'\ttimestamp: {self.timestamp}',
+     f'\tbody: {self.body}...'
+    ]).expandtabs(4)
+
+
+class NoticeFactory:
+  '''
+  Create Notice of KNU CSE official notice lists \n
+  example: https://computer.knu.ac.kr/06_sub/02_sub.html
+  '''
+  @staticmethod
+  def __from_notice_list_each(notice: Tag) -> 'Notice':
+    '''
+    create Notice from notice list in website \n
+    example: https://computer.knu.ac.kr/06_sub/02_sub.html
+    '''
+    [span.extract() for span in notice.select('span')]
+    url = Notice.post_url + notice.find('a')['href']
+    is_announcement = notice.find('th', attrs={'class': 'bbs_num'}).text == '공지'
+    return Notice(url, is_announcement)
+    
+  @staticmethod
+  def from_notice_list() -> list['Notice']:
+    '''
+    create a list of Notice from the knu cse notice list \n
+    example: https://computer.knu.ac.kr/06_sub/02_sub.html
+    '''
+    res = requests.get()
+    soup = BeautifulSoup(res.text, 'html.parser')
+    notices = soup.find_all('tr')  # 일반 공지사항 목록 가져옴
+
+    return [NoticeFactory.__from_notice_list_each(notice) for notice in notices]
